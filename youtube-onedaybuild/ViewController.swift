3a2b62d808930da5e86ca74759c912352abba796
ozgegundogdu <ozge.gundogdu@dk.akbank.com>
2020-07-29 14:12:32 +0300

    initial commit

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by L90025519MAC on 29.07.2020.
+//  Copyright © 2020 Ozge Gundogdu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

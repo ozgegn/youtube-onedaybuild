b4f9ce79498ff27cc9e30eb9e5dc6710b851af85
ozgegundogdu <ozge.gundogdu@dk.akbank.com>
2020-08-01 14:17:32 +0300

    constants is updated

--- youtube-onedaybuild/ConstantsTemplate.swift
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -1,16 +1,17 @@
 //
-//  Constants.swift
+//  ConstantsTemplate.swift
 //  youtube-onedaybuild
 //
-//  Created by L90025519MAC on 29.07.2020.
+//  Created by L90025519MAC on 1.08.2020.
 //  Copyright © 2020 Ozge Gundogdu. All rights reserved.
 //
 
 import Foundation
 
+/*
 struct Constants {
-    static var API_KEY = "AIzaSyDdWVwGVCM7nIB_BF6FIsF-Cf9M_3Qzu60"
-    static var PLAYLIST_ID = "PLMRqhzcHGw1aLoz4pM_Mg2TewmJcMg9ua"
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     
     
@@ -23,4 +24,4 @@ struct Constants {
         return nil
     }
     
-}
+}*/
